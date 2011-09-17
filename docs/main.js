7459baecb8e0ae88aa2091e2fdee90d7cedd501c
VILIC VANE <i@vilic.info>
2011-09-17 15:56:55 +0800

    document changes

--- docs/main.js
+++ docs/main.js
@@ -5,12 +5,11 @@
 	setInterval(resize, 10);
 
 	function resize() {
-		var nHeight = document.body.scrollHeight;
+		var nHeight = document.documentElement.scrollHeight;
 		if (nHeight == height) return;
 		height = nHeight;
 		var style = document.getElementById('page_wrapper').style;
 		style.height = 'auto';
 		style.height = height + 'px';
-		console.log(document.body.scrollHeight);
 	}
 };
